3d1f4e8d17f33d342d3e9793f7d3421ca128ddd1
Andrew Cummings <andrew.amalee@gmail.com>
2021-08-19 14:18:42 -0700

    more testing

--- src/components/Media.js
+++ src/components/Media.js
@@ -1,21 +1,26 @@
-import React from "react"
+import React from "react";
 
 export default function Media({ url }) {
-    if(`${url}`.includes('youtube') === true) {
-return <div className = "video-responsive">
-    <iframe
-    width="853"
-    height="480"
-    frameBorder="0"
-    src={`${url}`}
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowFullScreen
-    title="Embedded youtube" />;
-    </div>
-    } else {
-        return <div className= "image">
-            <img src={`${url}`}></img>
-        </div>
-    }
-
+  if (`${url}`.includes("youtube") === true) {
+    return (
+      <div className="video-responsive">
+        <iframe
+          width="853"
+          height="480"
+          frameBorder="0"
+          src={`${url}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="Embedded youtube"
+        />
+        ;
+      </div>
+    );
+  } else {
+    return (
+      <div className="image">
+        <img src={`${url}`} alt=""></img>
+      </div>
+    );
+  }
 }
